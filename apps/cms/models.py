0f8c9907488e9ed29b1edea923d9ae673031b8e9
json_xia <547816252@qq.com>
2020-02-19 23:11:17 +0800

    CMS权限限制

--- apps/cms/models.py
+++ apps/cms/models.py
@@ -3,6 +3,32 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
 
 
+class CmsPermission(object):
+    # 以255的二进制方式表示权限，1111 1111
+    ALL_PERMISSION = 0b11111111
+    # 1.访问者权限
+    VISITOR = 0b00000001
+    # 2.管理帖子的权限
+    POSTER = 0b00000010
+    # 3.管理评论的权限
+    COMMENTER = 0b00000100
+    # 4.管理板块的权限
+    BOARDER = 0b00001000
+    # 5.管理前台用户的权限
+    FRONTUSER = 0b00010000
+    # 6.管理后台用户的权限
+    CMSUSER = 0b00100000
+    # 7.最高权限
+    ADMINER= 0b01000000
+
+
+cms_role_user = db.Table(
+    'cms_role_user',
+    db.Column('cms_role_id',db.Integer,db.ForeignKey('cms_role.id'),primary_key=True),
+    db.Column('cms_user_id',db.Integer,db.ForeignKey('cms_user.id'),primary_key=True)
+)
+
+
 class CmsUser(db.Model):
     __tablename__ = 'cms_user'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
@@ -28,5 +54,39 @@ class CmsUser(db.Model):
         result = check_password_hash(self.password,raw_password)
         return result
 
+    @property
+    def permissions(self): # 通过这个函数，可以获取用户拥有的所有权限
+        if not self.roles:
+            return 0
+        # 如果用户有角色，再看他具有哪些权限
+        all_permissions = 0
+        for role in self.roles:
+            permissions = role. permissions
+            all_permissions |= permissions
+        return all_permissions
+
+
+    def has_permission(self,permission):
+        all_permissions = self.permissions
+        result = all_permissions&permission == permission
+        return result
+
+    @property
+    def is_develop(self):
+        return self.has_permission(CmsPermission.ALL_PERMISSION)
+
+
+class CmsRole(db.Model):
+    __tablename__ = 'cms_role'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name  = db.Column(db.String(50),nullable=True)
+    desc = db.Column(db.String(200),nullable=True)
+    permissions = db.Column(db.Integer,default=CmsPermission.VISITOR)
+    create_time = db.Column(db.DateTime,default=datetime.now)
+
+    users = db.relationship('CmsUser',secondary=cms_role_user,backref='roles')
+
+
 
 # 密码对外的字段叫做password，对内叫做_password
+
--- manage.py
+++ manage.py
@@ -6,6 +6,9 @@ from apps.cms import models as cms_models
 
 
 CmsUser = cms_models.CmsUser
+CmsRole = cms_models.CmsRole
+CmsPermission = cms_models.CmsPermission
+
 
 app = create_apps()
 manager = Manager(app)
@@ -20,6 +23,49 @@ def create_cms_user(username,password,email):
     db.session.add(user)
     db.session.commit()
     print("cms管理用户添加成功")
+
+
+@manager.command
+def create_cms_role():
+    # 访问者
+    visitor = CmsRole(name="访问者",desc="只能查看相关数据，不能修改",permissions=CmsPermission.VISITOR)
+    # 运营者
+    operator = CmsRole(name="运营者",desc="管理帖子，管理评论，管理前台用户",permissions=(
+        CmsPermission.VISITOR|CmsPermission.POSTER|CmsPermission.CMSUSER|CmsPermission.COMMENTER|
+    CmsPermission.FRONTUSER))
+    # 产品经理
+    productor = CmsRole(name="管理员", desc="拥有本系统所有权限", permissions=(
+            CmsPermission.VISITOR | CmsPermission.POSTER | CmsPermission.CMSUSER | CmsPermission.COMMENTER |
+            CmsPermission.FRONTUSER|CmsPermission.BOARDER))
+    # 开发者
+    developer = CmsRole(name="开发者", desc="开发者专用角色", permissions=CmsPermission.ALL_PERMISSION)
+
+    db.session.add_all([visitor,operator,productor,developer])
+    db.session.commit()
+    print("cms角色添加成功")
+
+@manager.option("-e","--email",dest="email")
+@manager.option("-n","--name",dest="name")
+def add_user_to_role(email,name):
+    user = CmsUser.query.filter_by(email=email).first()
+    if user:
+        role = CmsRole.query.filter_by(name=name).first()
+        if role:
+            role.users.append(user)
+            db.session.commit()
+            print("用户添加角色成功")
+        else:
+            print("没有这个角色:%s" %role)
+    else:
+        print("%s该用户不存在" %email)
+
+@manager.command
+def test_permission():
+    user = CmsUser.query.first()
+    if user.has_permission(CmsPermission.VISITOR):
+        print("")
+    else:
+        print("")
 
 
 if __name__ == '__main__':
